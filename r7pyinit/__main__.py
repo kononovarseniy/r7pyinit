287738feaa01bcfa4b0e359a0f2ed490ea9e475f
Arseniy Kononov <kononovarseniy@gmail.com>
2021-11-05 12:59:01 +0700

    Add entry point

--- r7pyinit/__main__.py
+++ r7pyinit/__main__.py
@@ -6,13 +6,19 @@ from r7pyinit.create_project import create_project
 PROGRAM = 'r7pyinit'
 DESCRIPTION = 'Create basic python project. Initializes git and poetry for development using vscode.'
 
-parser = argparse.ArgumentParser(prog=PROGRAM, description=DESCRIPTION, )
-parser.add_argument('project_name', type=str, help='Name of the project.')
-parser.add_argument('--path', type=Path, help='Root path of the project.')
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(prog=PROGRAM, description=DESCRIPTION, )
+    parser.add_argument('project_name', type=str, help='Name of the project.')
+    parser.add_argument('--path', type=Path, help='Root path of the project.')
 
-project_name: str = args.project_name
-root_path: Path = args.path or Path(args.project_name)
+    args = parser.parse_args()
 
-create_project(root_path, project_name)
+    project_name: str = args.project_name
+    root_path: Path = args.path or Path(args.project_name)
+
+    create_project(root_path, project_name)
+
+
+if __name__ == '__main__':
+    main()
