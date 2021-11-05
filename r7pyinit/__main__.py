b1f9d9c634a6bfd92fe405f620638f86eafc138e
Arseniy Kononov <kononovarseniy@gmail.com>
2021-11-05 12:18:47 +0700

    Improve cli arguments

--- r7pyinit/__main__.py
+++ r7pyinit/__main__.py
@@ -7,9 +7,12 @@ PROGRAM = 'r7pyinit'
 DESCRIPTION = 'Create basic python project. Initializes git and poetry for development using vscode.'
 
 parser = argparse.ArgumentParser(prog=PROGRAM, description=DESCRIPTION, )
-parser.add_argument('path', type=Path, help='Root path of the project.')
 parser.add_argument('project_name', type=str, help='Name of the project.')
+parser.add_argument('--path', type=Path, help='Root path of the project.')
 
 args = parser.parse_args()
 
-create_project(args.path, args.project_name)
+project_name: str = args.project_name
+root_path: Path = args.path or Path(args.project_name)
+
+create_project(root_path, project_name)
